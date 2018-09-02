7c62efc05133d076e373b67fecb0e81532a65a97
Glitch (fir-heart) <none>
2018-09-02 21:18:48 +0000

    🏙💖 Checkpoint ./.data/private-key.pem:68012/1679 ./index.js:68012/532 ./package.json:68012/271

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ module.exports = (app) => {
     // Post a comment on the issue
     return context.github.issues.createComment(params)
   })
+  
+  // example of probot responding 'Hello World' to a new PR being opened
+  app.on('pull_request.opened', async context => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    const params = context.issue({body: 'Hello World!'})
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
+
 }
