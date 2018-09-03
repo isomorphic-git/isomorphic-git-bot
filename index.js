53e3ad162ed0837d01b0c8db5aa0852a9cc70985
Glitch (isomorphic-git-bot) <none>
2018-09-03 20:54:13 +0000

    👖⛩ Checkpoint ./index.js:68012/750

--- index.js
+++ index.js
@@ -7,24 +7,9 @@ git.plugins.set('fs', fs)
 module.exports = (app) => {
   // Your code here
   app.log('Yay! The app was loaded!')
-
-  // example of probot responding 'Hello World' to a new issue being opened
-  app.on('issues.opened', async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({body: 'Hello World!'})
-
-    // Post a comment on the issue
-    return context.github.issues.createComment(params)
-  })
   
-  // example of probot responding 'Hello World' to a new PR being opened
   app.on('pull_request.opened', async context => {
     app.log(`Oooh! A PR was opened by ${context.payload.pull_request.user.login}!`)
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
     app.log(JSON.stringify(context, null, 2))
     const url = context.payload.pull_request.head.repo.clone_url
     const ref = context.payload.pull_request.head.ref
